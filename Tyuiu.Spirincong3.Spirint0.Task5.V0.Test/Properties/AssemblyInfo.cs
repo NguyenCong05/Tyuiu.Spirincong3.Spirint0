395b4057f758d6e603b006a8a48bd5c5bc1fb9f1
NguyenCong05 <Tyuiu.cong4@outlook.com>
2024-10-17 19:03:27 +0500

    Task5

---
+++ Tyuiu.Spirincong3.Spirint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.Spirincong3.Spirint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Tyuiu.Spirincong3.Spirint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6c09ed0-99ad-4a92-bf26-4cd6ca03e2e5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
